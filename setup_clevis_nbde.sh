b485e05aaad6ceb3620308085a436d1513be3266
stefano-ubuntuworkstation02 <stefano-ubuntuworkstation02@stefano.home>
2024-02-17 19:50:30 +0100

    Add support for CLEVIS Key Setup

---
+++ setup_clevis_nbde.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# If toolpath not set, set it to current working directory
+if [[ ! -v toolpath ]]
+then
+    toolpath=$(pwd)
+fi
+
+# Load configuration
+source $toolpath/config.sh
+
+# Update APT Lists
+apt-get update
+
+# Install clevis on the system and add clevis to the initramfs
+apt-get install --yes clevis clevis-luks clevis-initramfs cryptsetup-initramfs
+
+# Ask for password
+read -s -p "Enter encryption password: " password
+
+# For each keyserver
+keyservercounter=1
+for keyserver in "${keyservers[@]}"
+do
+     # Get TANG Server Key
+     curl -sfg http://$keyserver/adv -o /tmp/keyserver-$keyservercounter.jws
+
+     # For each disk device
+     for device in "${devices[@]}"
+
+	# Check which keys are currently used via CLEVIS
+     	list_device_keys=$(clevis luks list -d $device-part1)
+
+     	# Bind device to the TANG server via CLEVIS
+	if [[ "${list_device_keys}" == *"${keyserver}"* ]]; then
+        	echo "Keyserver <$keyserver> is already installed onto <$device> LUKS Header"
+     	else
+        	echo "Install Keyserver <$keyserver> onto <$device> LUKS Header"
+        	echo $password | clevis luks bind -d $device-part1 tang "{\"url\": \"http://$keyserver\" , \"adv\": \"/tmp/keyserver-$keyservercounter.jws\" }"
+     	fi
+
+	# Get information about LUKS and Clevis Keyslots
+	cryptsetup luksDump $device-part1
+	clevis luks list -d $device-part1
+     done
+
+     # Increment counter
+     keyservercounter=$((keyservercounter+1))
+done
+
+# Clear password from memory
+unset $password
+
+# Update initramfs
+update-initramfs -c -k all
+
