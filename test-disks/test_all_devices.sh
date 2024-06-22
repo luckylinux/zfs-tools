62e677ffbbd652bf86008bfc01f9ad71aec33018
luckylinux <luckylinux>
2024-06-22 20:10:57 +0200

    Using TMUX to launch different tests in different Windows.

--- test-disks/test_all_devices.sh
+++ test-disks/test_all_devices.sh
@@ -10,8 +10,21 @@ source ${toolpath}/config.sh
 # Create folder
 mkdir -p "${toolpath}/results/badblocks"
 
+# Init TMUX Session
+tmux new-session -d -s "testDevices" -n "General" "ls -l"
+
+# Initialize Counter
+counter=1
+
 # Loop over all Devices
 for device in "${disks[@]}"
 do
-     ${toolpath}/test-disk/test_single_device.sh "${device}"
+     # Create new TMUX Window
+     tmux new-window -t testDevices:${counter} -n ${device}
+
+     # Launch Script
+     tmux select-window -t testDevices:${counter} "${toolpath}/test-disk/test_single_device.sh \"aaaaaa\""
+
+     # Increase counter
+     counter=$(($counter+1))
 done
