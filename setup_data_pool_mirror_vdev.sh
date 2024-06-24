0adbd8386bdb9d0c6bf7167010594a6593a74bcf
luckylinux <luckylinux>
2024-06-24 17:40:43 +0200

    Use $poolname instead of hardcoded zdata as Pool Name.

--- setup_data_pool_mirror_vdev.sh
+++ setup_data_pool_mirror_vdev.sh
@@ -12,34 +12,34 @@ modprobe spl
 modprobe zfs
 
 # Create ZPOOL and add first VDEV
-zpool create -f -o ashift=12 -O compression=lz4 -m none zdata mirror "/dev/mapper/${luksdevices[0]}" "/dev/mapper/${luksdevices[1]}"
+zpool create -f -o ashift=12 -O compression=lz4 -m none ${poolname} mirror "/dev/mapper/${luksdevices[0]}" "/dev/mapper/${luksdevices[1]}"
 
 # Wait a bit
 sleep 5
 
 # Add second VDEV
-zpool add -o ashift=12 zdata mirror "/dev/mapper/${luksdevices[2]}" "/dev/mapper/${luksdevices[3]}"
+zpool add -o ashift=12 ${poolname} mirror "/dev/mapper/${luksdevices[2]}" "/dev/mapper/${luksdevices[3]}"
 
 # Wait a bit
 sleep 5
 
 # Add third VDEV
-#zpool add -o ashift=12 zdata mirror "/dev/mapper/${luksdevices[4]}" "/dev/mapper/${luksdevices[5]}"
+#zpool add -o ashift=12 ${poolname} mirror "/dev/mapper/${luksdevices[4]}" "/dev/mapper/${luksdevices[5]}"
 
 # Wait a bit
 #sleep 5
 
 # Add fourth VDEV
-#zpool add -o ashift=12 zdata mirror "/dev/mapper/${luksdevices[6]}" "/dev/mapper/${luksdevices[7]}"
+#zpool add -o ashift=12 ${poolname} mirror "/dev/mapper/${luksdevices[6]}" "/dev/mapper/${luksdevices[7]}"
 
 # Wait a bit
 #sleep 5
 
 # Add fourth VDEV
-#zpool add -o ashift=12 zdata mirror "/dev/mapper/${luksdevices[8]}" "/dev/mapper/${luksdevices[9]}"
+#zpool add -o ashift=12 ${poolname} mirror "/dev/mapper/${luksdevices[8]}" "/dev/mapper/${luksdevices[9]}"
 
 # Wait a bit
 #sleep 5
 
 # Add fourth VDEV
-#zpool add -o ashift=12 zdata mirror "/dev/mapper/${luksdevices[10]}" "/dev/mapper/${luksdevices[11]}"
+#zpool add -o ashift=12 ${poolname} mirror "/dev/mapper/${luksdevices[10]}" "/dev/mapper/${luksdevices[11]}"
