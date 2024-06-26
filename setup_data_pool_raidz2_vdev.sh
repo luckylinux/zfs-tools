11d2e49889761c8feac9c04a5551c60fe1e0618c
luckylinux <luckylinux>
2024-06-26 07:33:25 +0200

    Use ashift Variable defined in config.sh when setting up Pool.

--- setup_data_pool_raidz2_vdev.sh
+++ setup_data_pool_raidz2_vdev.sh
@@ -12,10 +12,10 @@ modprobe spl
 modprobe zfs
 
 # Create ZPOOL and add First 6-Disk RAIDZ2 VDEV
-zpool create -f -o ashift=12 -O compression=lz4 -m none ${poolname} raidz2 "/dev/mapper/${luksdevices[0]}" "/dev/mapper/${luksdevices[1]}" "/dev/mapper/${luksdevices[2]}" "/dev/mapper/${luksdevices[3]}" "/dev/mapper/${luksdevices[4]}" "/dev/mapper/${luksdevices[5]}"
+zpool create -f -o ashift=${ashift} -O compression=lz4 -m none ${poolname} raidz2 "/dev/mapper/${luksdevices[0]}" "/dev/mapper/${luksdevices[1]}" "/dev/mapper/${luksdevices[2]}" "/dev/mapper/${luksdevices[3]}" "/dev/mapper/${luksdevices[4]}" "/dev/mapper/${luksdevices[5]}"
 
 # Wait a bit
 sleep 5
 
 # Add Second 6-Disk RAIDZ2 VDEV
-zpool add -o ashift=12 ${poolname} raidz2 "/dev/mapper/${luksdevices[6]}" "/dev/mapper/${luksdevices[7]}" "/dev/mapper/${luksdevices[8]}" "/dev/mapper/${luksdevices[9]}" "/dev/mapper/${luksdevices[10]}" "/dev/mapper/${luksdevices[11]}"
+zpool add -o ashift=${ashift} ${poolname} raidz2 "/dev/mapper/${luksdevices[6]}" "/dev/mapper/${luksdevices[7]}" "/dev/mapper/${luksdevices[8]}" "/dev/mapper/${luksdevices[9]}" "/dev/mapper/${luksdevices[10]}" "/dev/mapper/${luksdevices[11]}"
