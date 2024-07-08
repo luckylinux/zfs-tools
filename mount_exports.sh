116e985b65b9c7bb27d70f55cd6614fdcf66d6ae
luckylinux <luckylinux>
2024-07-08 06:23:25 +0200

    Added Poolname Argument to mount_exports.sh.

--- mount_exports.sh
+++ mount_exports.sh
@@ -4,6 +4,9 @@
 relativepath="./" # Define relative path to go from this script to the root level of the tool
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
 
+# Pool name
+pool=${1:-"zdata"}
+
 # Load configuration
 source "${toolpath}/load.sh"
 
